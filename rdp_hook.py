18f53f3c5cd9e326b2d884cc441cd8c6cd75e05a
admin <Wuyanni@2021>
2022-11-02 15:20:51 +0800

    push

---
+++ rdp_hook.py
@@ -0,0 +1,20 @@
+def Hhook(target_process):
+ try:
+     session = frida.attach(target_process)
+     script = session.create_script("""
+         var clipbrd = Module.getExportByName("user32.dll", "GetClipboardData");
+         Interceptor.attach(clipbrd, {
+                 onEnter: function (args) {
+                 },
+                 onLeave: function (retval, state) {
+                     retval.replace(ptr(NULL));
+                 },
+         });
+         """)
+     script.on('message', on_message)
+     script.load()
+     str="hook %s success!\n"%target_process
+     print("hook %s success!"%target_process)
+ except Exception as e: 
+  print(f'hook {target_process} error:{e}')
+ 
